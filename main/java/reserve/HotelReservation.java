59337b11099368be73827f5b7fd087703d41d560
saudhasan762 <azanali152@gmail.com>
2021-03-06 00:15:49 +0530

    UC6 - Finding Cheapest highest rated hotels

--- main/java/reserve/HotelReservation.java
+++ main/java/reserve/HotelReservation.java
@@ -6,42 +6,10 @@ public class HotelReservation {
 
 
     public static void main(String[] args) {
-        String hotel1 = "Lakewood";
-        String hotel2 = "Bridgewood";
-        String hotel3 = "RidgeWood";
-        int LakeWeekday = 110, LakeWeekend = 90;
-        int BridWeekday = 150, BridWeekend = 50;
-        int RidWeekday = 220, RidWeekend = 150;
-        int ratingOfLake = 3, ratingOfBridge = 4, ratingOfRidge = 5;
-        Scanner scanner = new Scanner(System.in);
         System.out.println("! Welcome to Hotel Management System !");
         Reservation reservation = new Reservation();
-        System.out.println("Enter 1 to view Hotels");
-        System.out.println("Enter 2 to view cheapest Hotel");
-        int view = scanner.nextInt();
-        if (view == 1) {
-            reservation.addDetails(hotel1, LakeWeekday, LakeWeekend,ratingOfLake,110);
-            reservation.addDetails(hotel2, BridWeekday, BridWeekend, ratingOfBridge,160);
-            reservation.addDetails(hotel3, RidWeekday, RidWeekend, ratingOfRidge,220);
-            reservation.viewDetails();
-        } else if (view == 2) {
-            System.out.println("Enter the number of Days to reserve");
-            int numOfDays = scanner.nextInt();
-            System.out.println("Enter 1 for weekday");
-            System.out.println("Enter 2 for weekend");
-            int choice = scanner.nextInt();
-            if (choice == 1) {
-                reservation.addDetails(hotel1, LakeWeekday, LakeWeekend,ratingOfLake, numOfDays * LakeWeekday);
-                reservation.addDetails(hotel2, BridWeekday, BridWeekend, ratingOfBridge,numOfDays * BridWeekday);
-                reservation.addDetails(hotel3, RidWeekday, RidWeekend,ratingOfRidge, numOfDays * RidWeekday);
-                reservation.showCheapestHotel();
-            } else if (choice == 2) {
-                reservation.addDetails(hotel1, LakeWeekday, LakeWeekend,ratingOfLake, numOfDays * LakeWeekend);
-                reservation.addDetails(hotel2, BridWeekday, BridWeekend,ratingOfBridge, numOfDays * BridWeekend);
-                reservation.addDetails(hotel3, RidWeekday, RidWeekend,ratingOfLake, numOfDays * RidWeekend);
-                reservation.showCheapestHotel();
-            } else
-                System.out.println("Invalid Choice");
+        while (true) {
+            reservation.getDetails();
         }
     }
  }
--- main/java/reserve/Reservation.java
+++ main/java/reserve/Reservation.java
@@ -21,6 +21,37 @@ public class Reservation {
         }
     }
 
+    public boolean getDetails(){
+        String hotel1 = "Lakewood";
+        String hotel2 = "Bridgewood";
+        String hotel3 = "RidgeWood";
+        int LakeWeekday = 110, LakeWeekend = 90;
+        int BridWeekday = 150, BridWeekend = 50;
+        int RidWeekday = 220, RidWeekend = 150;
+        int ratingOfLake = 3, ratingOfBridge = 4, ratingOfRidge = 5,total = 0;
+        System.out.println("Enter the number of Days to reserve");
+        Scanner scanner = new Scanner(System.in);
+        int numOfDays = scanner.nextInt();
+        System.out.println("Enter 1 for weekday");
+        System.out.println("Enter 2 for weekend");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            addDetails(hotel1, LakeWeekday, LakeWeekend,ratingOfLake, numOfDays * LakeWeekday);
+            addDetails(hotel2, BridWeekday, BridWeekend, ratingOfBridge,numOfDays * BridWeekday);
+            addDetails(hotel3, RidWeekday, RidWeekend,ratingOfRidge, numOfDays * RidWeekday);
+            showCheapestHotel();
+        } else if (choice == 2) {
+            addDetails(hotel1, LakeWeekday, LakeWeekend,ratingOfLake, numOfDays * LakeWeekend);
+            addDetails(hotel2, BridWeekday, BridWeekend,ratingOfBridge, numOfDays * BridWeekend);
+            addDetails(hotel3, RidWeekday, RidWeekend,ratingOfLake, numOfDays * RidWeekend);
+            showCheapestHotel();
+        } else {
+            System.out.println("Invalid Choice");
+            return false;
+        }
+        return true;
+    }
+
     public void showCheapestHotel(){
         System.out.println("Cheapest Hotel: "+getCheapestHotel());
     }
